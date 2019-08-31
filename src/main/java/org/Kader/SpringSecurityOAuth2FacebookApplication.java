a4a9aba9acd6760754ca265aa1aff59ba69f8227
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-08-31 17:30:17 +0200

    add configuration from connexion in facebookaccount

---
+++ src/main/java/org/Kader/SpringSecurityOAuth2FacebookApplication.java
@@ -0,0 +1,13 @@
+package org.Kader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2FacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuth2FacebookApplication.class, args);
+	}
+
+}
